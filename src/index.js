ec781adb721dd58a1a671c9bc40a8773e80c1a0d
TP00214906 <gulati.parth98@gmail.com>
2022-07-22 21:02:57 +0530

    Setup redux for the project state management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "LOGGED_IN_USER":
+      return { ...state, ...action.payload };
+    case "LOGOUT":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+
+const store = configureStore(rootReducer, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
