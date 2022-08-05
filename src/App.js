22d0a07324b4d5701e2f355d01edf47f07990b67
Pattypan <gulati.parth98@gmail.com>
2022-08-06 02:00:42 +0530

    Added .env file to the project, completed the registration flow with toaster notifications and everything

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Login from "./auth/Login";
 import Register from "./auth/Register";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import TopNav from "./components/TopNav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <TopNav/>
+      <ToastContainer/>
       <Routes>
         <Route path="/" element={<Home/>} /> 
         <Route path="/login" element={<Login/>} /> 
--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 import RegisterForm from "../components/RegisterForm";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`http://localhost:8000/api/register`, {
+      const res = await axios.post(`${process.env.REACT_APP_API}/register`, {
         name,
         email,
         password,
       });
 
-      console.log(res);
+      toast.success("User successfully registered, please login to continue");
+      navigate("/login");
     } catch (err) {
       console.log(err);
+      if (err.response.status === 400) toast.error(err.response.data);
     }
   };
 
@@ -27,7 +32,6 @@ const Register = () => {
       <div className="container-fluid bg-secondary p-5 text-center">
         <h1>Register</h1>
       </div>
-
       <div className="container">
         <div className="row">
           <div className="col-md-6 offset-md-3">
